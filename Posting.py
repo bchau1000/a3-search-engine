7c6820f8b8a248a0310c2b45c62c4a16b45c8c35
Farel Harimawan <fharimawan@gmail.com>
2020-11-12 14:30:58 -0800

    removed url from posting

--- Posting.py
+++ Posting.py
@@ -1,10 +1,9 @@
 class Posting:
     def __init__(self, docID: int, url: str, freq: int):
         self.id = docID
-        self.url = url
+        # self.url = url
         self.freq = freq
 
     # Overriden __str__ to make it easier to debug when writing to file/console/etc
-    # NOTICE: I do not print the url since it just clutters up the already massive index
     def __str__(self):
         return f'({self.id}, {self.freq})'
