56a0cf793283494ca35f375aef1e336ac4113f78
Farel Harimawan <fharimawan@gmail.com>
2020-11-13 16:45:41 -0800

    changed freq attribute to tf_idf

--- Posting.py
+++ Posting.py
@@ -1,12 +1,12 @@
 class Posting:
-    def __init__(self, docID: int, freq: int):
+    def __init__(self, docID: int, tf_idf: int):
         self.id = docID
         # self.url = url
-        self.freq = freq
+        self.tf_idf = tf_idf
 
     # Overriden __str__ and __repr__ to make it easier to debug when writing to file/console/etc
     def __str__(self):
-        return f'({self.id}, {self.freq})'
+        return f'({self.id}, {self.tf_idf})'
 
     def __repr__(self):
-        return f'Posting({self.id}, {self.freq})'
+        return f'Posting({self.id}, {self.tf_idf})'
