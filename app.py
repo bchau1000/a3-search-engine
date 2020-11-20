eb3ab6aa57c8cdbfccb1b427fd78fa3da280e464
bchau1000 <bchau1000@gmail.com>
2020-11-20 09:32:44 -0800

    Added comments/documentation

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, render_template, request, url_for
 
 app = Flask(__name__)
 
-# Comment this out if you're just testing for front-end changes
+# Preload required pickle data for the inverted index
 print('Preloading pickled data')
 with open('lexicon.pkl', 'rb') as f:
     lexicon = pickle.load(f)
@@ -17,31 +17,47 @@ with open('corpus.pkl', 'rb') as f:
 
 print('Finished!')
 
+# Register '/' homepage path as search.html
 @app.route("/")
 def search():
     return render_template('search.html')
 
+# Register '/results' path as results.html
 @app.route("/results")
 def results():
     return render_template('results.html')
 
+# Register '/api/results' for frontend/backend communication
 @app.route("/api/results")
 def handle_results():
+    # Grab query parameters from the url
+    # e.g. /results?query=machine+learning
+        # returns 'machine learning'
     query = request.args.get('query')
 
+    # Call QueryProcessor.search to perform search on index
     start_time = time.perf_counter()
     results = QueryProcessor.search(query, lexicon, corpus)
     stop_time = time.perf_counter()
     print(f'found {len(results)} in {stop_time - start_time}')
 
-    json_results = list()
+    # Initialize a list for our JSON objects, this will serve as our JSON array
+    json_array = list()
 
+    # For each url in results create a JSON object using a dictionary
+        # Each object should be in the format {'url':'https://www.google.com'}
+        # Append each of these objects to our JSON array
+        # Formatting the results like this is necessary to use jsonify()
     for url in results[:5]:
         json_object = dict()
         json_object["url"] = str(url)
-        json_results.append(json_object)
-    
-    return jsonify(json_results)
+        json_array.append(json_object)
+
+    # Turn our JSON array into an actual JSON array using jsonify()
+    # You can see the output format by adding a path to the url, eg.:
+        # Original query is: http://127.0.0.1:5000/results?query=machine+learning
+             # Change it to: http://127.0.0.1:5000/api/results?query=machine+learning to see JSON output
+    return jsonify(json_array)
 
 if __name__ == "__main__":
     app.run(use_reloader=False)
--- static/js/results.js
+++ static/js/results.js
@@ -1,18 +1,22 @@
+// Grab the query entered in the search bar from the url
 const query_string = window.location.search.substr(1);
 
 function populate_page(response)
 {
+    // Grab the search tokens to output: 
+        // Results for 'search_tokens'
     var search_tokens = query_string.substr(6).replace('+', ' ')
     $('#results_header').append("'" + search_tokens + "'")
 
-    // Parse the JSON array from response, list out the urls
+    // Parse the JSON array from response, list out the urls as links
     for(let i = 0; i < response.length; i++)
         $('#url_table').append('<a href="' + response[i]['url'] + '"> ' + response[i]['url'] + '</a>' + '<br>');
 }
 
 $(document).ready(function(){
-    // Grab the query from the url, send a request to api/results
-    // Should return a response in the form of a JSON array
+    // Send a request to api/results with our query
+    // Should return a response in the form of a JSON array, as defined in app.py
+        // Pass the JSON array from the response to populate_page(response)
     $.ajax({
         url: 'api/results',
         data: query_string,
