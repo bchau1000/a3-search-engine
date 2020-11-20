071452232ceb3c75da8eaf87bedb540da5f9df6c
Farel Harimawan <fharimawan@gmail.com>
2020-11-19 20:29:35 -0800

    implmented new driver

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import pickle
 from pathlib import Path
-from Index import Indexer
+import Index
 import time
 from QueryProcessor import QueryProcessor
-import os
 
 
 def main():
     rootDir = 'DEV'
-    if not Path('index.pickle').exists():
-        inverted_index = Indexer.create_index(rootDir)
-    else:
-        # Unpickle the inverted index here for testing purposes
-        print('Unpickling index...')
-        inverted_index = pickle.load(open('index.pickle', 'rb'))
-        print('Index unpickled')   
-    
-    corpus = [fname for dirName, subdirList, fileList in os.walk(rootDir) for fname in fileList]
+    # if the index doesn't exist yet creat it!
+    if not Path('index.txt').exists():
+        Index.Indexer.create_index(rootDir)
 
+    ##### AUXILIARY STRUCTURES #####
+
+    # lexicon
+    if not Path('lexicon.pkl').exists():
+        lexicon = Index.load_lexicon()
+        with open('lexicon.pkl', 'wb') as f:
+            pickle.dump(lexicon, f)
+    else:
+        with open('lexicon.pkl', 'rb') as f:
+            lexicon = pickle.load(f)
+
+    # corpus
+    if not Path('corpus.pkl').exists():
+        corpus = Index.load_url_lookup(rootDir)
+        with open('corpus.pkl', 'wb') as f:
+            pickle.dump(corpus, f)
+    else:
+        with open('corpus.pkl', 'rb') as f:
+            corpus = pickle.load(f)
+    
+    ##### DRIVER #####
     while True:
         query = input('Enter query (-1 to stop):')
         if query == '-1':
             break
         start_time = time.perf_counter()
-        results = QueryProcessor.search(query, inverted_index, corpus)
+        results = QueryProcessor.search(query, lexicon, corpus)
         stop_time = time.perf_counter()
         print(f'found {len(results)} in {stop_time - start_time}s')
         for i in range(5):
